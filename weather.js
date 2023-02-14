29cf5b930fabdb645f55e09ff37c9045a2d785c8
mohitsonu <mohitsonu33@gmail.com>
2023-02-14 23:45:38 +0530

    Add files via upload
    Signed-off-by: mohitsonu <mohitsonu33@gmail.com>

---
+++ weather.js
@@ -0,0 +1,40 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4cf13224ecmsh0240cd265ce3f0fp141f25jsn21be6685ef56",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const getWeather  = (city)=>
+{
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+  .then((response) => response.json())
+  .then((response) => {
+
+    console.log(response);
+    cityname.innerHTML = city
+    cloud_pct.innerHTML = response.cloud_pct
+    temp.innerHTML = response.temp
+    temp2.innerHTML = response.temp
+    feels_like.innerHTML = response.feels_like
+    humidity.innerHTML = response.humidity
+    humidity2.innerHTML = response.humidity
+    min_temp.innerHTML = response.min_temp
+    max_temp.innerHTML = response.max_temp
+    wind_speed.innerHTML = response.wind_speed
+    wind_speed2.innerHTML = response.wind_speed
+    wind_degrees.innerHTML = response.wind_degrees
+    sunrise.innerHTML = response.sunrise
+    sunset.innerHTML = response.sunset
+  })
+
+    .catch(err => console.error(err));
+}
+  submits.addEventListener("click", (e) => {
+  e.preventDefault()
+  getWeather(city.value)
+})
+
+getWeather("Kakinada")
